7a806ae6518bb95a723e60a8b008ee227fe731df
Mohamed FQUIHI <mohamed.fquihi@atos.net>
2023-10-13 17:09:56 +0100

    adding Customers list to cutomers component

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+    selector: 'app-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ["customer.component.scss"]
 })
 export class CustomerComponent {
+    customers: Customer[] = [];
+    newCustomer: Customer = new Customer('', '', '', '');
 
+    constructor() {
+        this.newCustomer = new Customer('', '', '', '');
+    }
+
+    addCustomer() {
+        if (this.newCustomer.firstName && this.newCustomer.lastName && this.newCustomer.email) {
+            this.customers.push(this.newCustomer);
+            this.newCustomer = new Customer('', '', '', ''); // Clear the form
+        }
+    }
+
+    deleteCustomer(customer: Customer) {
+      const index = this.customers.indexOf(customer);
+      if (index !== -1) {
+          this.customers.splice(index, 1);
+      }
+  }
+
+  
+}
+
+export class Customer {
+    constructor(public firstName: string, public lastName: string, public email: string, public password: string) {}
 }
