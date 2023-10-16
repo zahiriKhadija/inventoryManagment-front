60566b4a7908497708178a3fef7b71856c1d25c4
zahiriKhadija <khadija.zahiri@atos.net>
2023-10-16 14:11:54 +0100

    add order details

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-details',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-details.component.scss']
 })
 export class OrderDetailsComponent {
+
+  constructor(
+    private readonly router: Router,
+  ) { }
+
+  public retour()
+  {
+    this.router.navigate(['/order']);
+  }
+
 
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
+
 export class OrderComponent {
+  constructor(
+    private readonly router: Router,
+  ) { }
+
+  public afficherDetails()
+  {
+    this.router.navigate(['/order-details']);
+  }
+
 
 }
