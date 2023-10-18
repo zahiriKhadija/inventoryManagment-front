3ab77009dde63ca36d000c462855c7249640f62e
zahiriKhadija <khadija.zahiri@atos.net>
2023-10-18 10:38:54 +0100

    add services

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Environment } from '../environment/environment';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+
+  constructor(
+    private readonly http: HttpClient,
+    private readonly PRODUCT_PORT : "8085",
+  ) { }
+
+
+  public getProducts():Observable<Array<Product>>{
+    return this.http.get<Array<Product>>(Environment.backendHost + this.PRODUCT_PORT + "/products")
+  }
+ 
+  
+}
