971f411c94d7e671f56ab41b6a3bee270cda52e4
zahiriKhadija <khadija.zahiri@atos.net>
2023-10-19 12:15:29 +0100

    service order

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -8,14 +8,30 @@ import { Environment } from '../environment/environment';
   providedIn: 'root'
 })
 export class OrderService {
+  
 
   constructor(
     private readonly http: HttpClient,
-    private readonly PRODUCT_PORT : "8085",
+    private readonly ORDER_PATH : "/orders",
   ) { }
 
-
   public getOrders():Observable<Array<Order>>{
-    return this.http.get<Array<Order>>(Environment.backendHost + this.PRODUCT_PORT + "/orders")
+    return this.http.get<Array<Order>>(Environment.backendHost + this.ORDER_PATH + "/all")
+  }
+
+  public getOrdersByIdUser():Observable<Array<Order>>{
+    return this.http.get<Array<Order>>(Environment.backendHost + this.ORDER_PATH + "/byIdUser")
+  }
+
+  public createOrder(order: Order):Observable<Order>{
+    return this.http.post<Order>(Environment.backendHost + this.ORDER_PATH + "/createOrder",order);
+  }
+
+  public updateOrder(order: Order):Observable<Order>{
+    return this.http.put<Order>(Environment.backendHost + this.ORDER_PATH + "/updateOrder",order);
+  }
+
+  public deleteOrder(id: number){
+    return this.http.delete(Environment.backendHost + this.ORDER_PATH + "/deleteOrder/"+id);
   }
 }
