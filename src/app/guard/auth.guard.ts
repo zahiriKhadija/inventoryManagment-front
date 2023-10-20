a1e3a066aeb288d600e3b486752bbf44f8955e80
Asmaâ ELABID <asmaa.elabid@atos.net>
2023-10-20 18:50:11 +0100

    security suite

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,12 @@
+import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot,Router } from '@angular/router';
 
 
 
-const router= new Router();
+
 export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const token = localStorage.getItem('token');
+  const router = inject(Router);
   if(token){
   return true;
   }
